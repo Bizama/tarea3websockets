c7c7776d178db693cd1766c3e05781f3f9665726
Javier Bizama <javierbizama@Javiers-MacBook-Pro.local>
2021-05-24 01:17:38 -0400

    chat listo

--- t3/src/components/HelloWorld.vue
+++ t3/src/components/HelloWorld.vue
@@ -1,30 +1,90 @@
 <template>
-  <span>Primer deploy listo</span>
+  <div>
+    <h1>Websocket Airlines Information</h1>
+    <div> 
+      <ul ref="chatbox" class="chatbox-container" id="chatbox">
+        <li v-for="message in messages" :key="message.name" class="list">
+          {{ message.name }}({{message.date}}): {{ message.message }}
+        </li>
+      </ul>
+      <form @submit.prevent="sendMessage" class="chatbox-bottom" v-if="this.nickname != ''">
+        <input  class="chatbox-input" type="text" placeholder="Message..." v-model="message" />
+        <input type="submit" value="Send" />
+      </form>
+      <form @submit.prevent="changeNickname" v-else>
+        <span>Introduzca un nickname para chatear</span>
+        <br>
+        <input type="text"  class="chatbox-input" placeholder="Escriba su nickname..." v-model="nickname_aux" />
+        <input type="submit" value="Ingresar" /> 
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import io from "socket.io-client";
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      socket: {},
+      message: '',
+      messages: [],
+      nickname: '',
+      flights: [],
+      planes: [],
+    }
+  },
+  created() {
+    this.socket = io("wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl", {path: '/flights'});
+  },
+  mounted() {
+    this.socket.on('POSITION', data => {
+      console.log(data);
+    });
+    this.socket.on('CHAT', data => {
+      const message_date = new Date(data.date);
+      data.date = message_date;
+      this.messages.push(data);
+      this.$nextTick(function() {
+        var messageBox = this.$refs['chatbox'];
+        messageBox.scrollTop = messageBox.scrollHeight;
+        }); 
+    });
+  },
+  methods: {
+    sendMessage: function () {
+      this.socket.emit('CHAT', {name: this.nickname, message: this.message} );
+      this.message = ''
+      },
+    checkNickname: function() { 
+      if (this.nickname == '') {
+        return false;
+      }
+    },
+    changeNickname: function() {
+      this.nickname = this.nickname_aux 
+    },
   }
-}
+  
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.chatbox-container{
+    height: 50px;
+    width: 400px;
+    overflow-y: scroll;
+  }
+
+.chatbox-input {
+  margin-left:16px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.chatbox-bottom {
+  width: 400px;
+  justify-content: space-between;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
